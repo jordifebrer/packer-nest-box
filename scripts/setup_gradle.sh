4e8a432e13df5753c31b2ef28969f26d742ceed2
Jordi Febrer <jordi.febrer@gmail.com>
2015-05-09 19:34:52 +0100

    Added Gradle setup script

---
+++ scripts/setup_gradle.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd /opt/
+wget https://services.gradle.org/distributions/gradle-2.4-bin.zip
+unzip gradle-2.4-bin.zip
+
+/bin/cat << EOF >> /etc/bashrc
+export GRADLE_HOME=/opt/gradle-2.4
+export PATH=$PATH:/opt/gradle-2.4/bin
