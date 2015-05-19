c8241a3d0b8d26a299c50275cee416b99595414e
Jordi Febrer <jordi.febrer@gmail.com>
2015-05-19 15:32:00 +0100

    Moved Maven dependency out of the java_setup script

--- scripts/setup_java.sh
+++ scripts/setup_java.sh
@@ -9,23 +9,3 @@ export JAVA_HOME=/opt/jdk1.7.0_79
 export JRE_HOME=/opt/jdk1.7.0_79/jre
 export PATH=$PATH:/opt/jdk1.7.0_79/bin:/opt/jdk1.7.0_79/jre/bin
 EOF
-
-touch /etc/yum.repos.d/apache-maven.repo
-
-/bin/cat << EOF > /etc/yum.repos.d/apache-maven.repo
-[epel-apache-maven]
-name=maven from apache foundation.
-baseurl=http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-6/x86_64/
-enabled=1
-skip_if_unavailable=1
-gpgcheck=0
-
-[epel-apache-maven-source]
-name=maven from apache foundation. - Source
-baseurl=http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-6/SRPMS
-enabled=0
-skip_if_unavailable=1
-gpgcheck=0
-EOF
-
-yum install -y apache-maven
