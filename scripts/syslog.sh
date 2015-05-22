d63db4736d32f1408973a2689c55e083306ecdcf
Jordi Febrer <jordi.febrer@gmail.com>
2015-05-22 16:02:00 +0100

    Create syslog.sh

---
+++ scripts/syslog.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+yum install -y rsyslog
+
+bin/cat << EOF >> /etc/rsyslog.conf
+# Provides UDP syslog reception
+$ModLoad imudp
+$UDPServerRun 514
+ 
+# Provides TCP syslog reception
+$ModLoad imtcp
+$InputTCPServerRun 514
+EOF
+
+touch /etc/rsyslog.d/user.conf
+
+bin/cat << EOF > /etc/rsyslog.d/user.conf
+user.*          /var/log/user.log
+user.*          @remotelogserver
+EOF
+
+sudo service rsyslog restart
+
+# logger -p user.info "test message"
