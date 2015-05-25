083def635e59db068c316c75fd9f68a30c6701d7
Jordi Febrer <jordi.febrer@gmail.com>
2015-05-25 10:17:23 +0100

    Update docker.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,3 +2,17 @@
 
 rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 yum -y install docker-io
+
+# check if the docker group exists
+# cat /etc/group | grep docker
+
+# create a docker group
+# sudo groupadd docker
+
+# add packer user the docker group in order to avoid sudo
+sudo usermod -g docker packer
+
+# lists packer user groups
+# groups packer
+
+# reboot
