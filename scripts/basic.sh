4332c5dbe7636299812f4ef7a4561c3e60e539c7
Jordi Febrer <jordi.febrer@gmail.com>
2015-06-03 14:37:07 +0100

    Update basic.sh

--- scripts/basic.sh
+++ scripts/basic.sh
@@ -2,4 +2,5 @@
 
 yum groupinstall -y development # or yum -y groupinstall "development tools"
 yum -y install vim wget unzip
+yum -y install epel-release
 yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel openldap-devel libxml2-devel libxslt-devel
